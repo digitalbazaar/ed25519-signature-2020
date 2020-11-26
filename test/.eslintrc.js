199cf4c2982084ae2e7f06b5c4c0845a1f5167c5
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-11-25 22:06:21 -0500

    Initial commit.

---
+++ test/.eslintrc.js
@@ -0,0 +1,16 @@
+/*!
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+module.exports = {
+  globals: {
+    should: true,
+    assertNoError: true
+  },
+  env: {
+    node: true,
+    mocha: true
+  },
+  extends: [
+    'digitalbazaar'
+  ]
+};
---
+++ test/mock-data.js
@@ -0,0 +1,12 @@
+/*!
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+export const mockKey = {
+  type: 'Ed25519VerificationKey2020',
+  publicKeyMultibase: 'zEYJrMxWigf9boyeJMTRN4Ern8DJMoCXaLK77pzQmxVjf',
+  privateKeyMultibase: 'z4E7Q4neNHwv3pXUNzUjzc6TTYspqn9Aw6vakpRKpbVrCzwKWD4hQ' +
+    'DHnxuhfrTaMjnR8BTp9NeUvJiwJoSUM6xHAZ'
+};
+
+export const seed =
+  '8c2114a150a16209c653817acc7f3e7e9c6c6290ae93d6689cbd61bb038cd31b';
