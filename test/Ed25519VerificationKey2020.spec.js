41f186126fbaaca7429d3f836f77071b193d02d9
Dmitri Zagidulin <dzagidulin@gmail.com>
2021-02-26 00:04:10 -0500

    Update to use new 2020 context.

--- test/Ed25519VerificationKey2020.spec.js
+++ test/Ed25519VerificationKey2020.spec.js
@@ -1,13 +1,12 @@
 /*!
- * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2020-2021 Digital Bazaar, Inc. All rights reserved.
  */
-import jsigs from 'jsonld-signatures';
-const {purposes: {AssertionProofPurpose}} = jsigs;
-import securityV3 from './security-v3.js';
-
 import chai from 'chai';
 chai.should();
 const {expect} = chai;
+
+import jsigs from 'jsonld-signatures';
+const {purposes: {AssertionProofPurpose}} = jsigs;
 
 import {Ed25519VerificationKey2020} from
   '@digitalbazaar/ed25519-verification-key-2020';
@@ -19,46 +18,25 @@ import {
   contexts,
 } from '@transmute/jsonld-document-loader';
 
-// const securityV1 = contexts
-//   .W3ID_Security_Vocabulary['https://w3id.org/security/v1'];
-const securityV2 = contexts
-  .W3ID_Security_Vocabulary['https://w3id.org/security/v2'];
-// // Hideously modify to include this new Ed25519Signature2020 suite
-// securityV1['@context'].Ed25519Signature2020 =
-//   securityV1['@context'].Ed25519Signature2018;
+import * as ed25519 from 'ed25519-signature-2020-context';
+
+// const securityV2 = contexts
+//   .W3ID_Security_Vocabulary['https://w3id.org/security/v2'];
 
 const documentLoader = documentLoaderFactory.pluginFactory
   .build({
     contexts: {
       ...contexts.W3C_Verifiable_Credentials,
       // ...contexts.W3ID_Security_Vocabulary,
-      // 'https://w3id.org/security/v1': securityV1,
-      'https://w3id.org/security/v2': securityV2,
-      'https://w3id.org/security/v3': securityV3
-    },
+      // 'https://w3id.org/security/v2': securityV2,
+      'https://w3id.org/security/ed25519-signature-2020/v1': ed25519
+        .contexts.get('https://w3id.org/security/ed25519-signature-2020/v1')
+    }
   })
-  // .addContext(contexts.W3C_Decentralized_Identifiers)
   .addContext({
     [mockKey.controller]: controllerDoc
   })
   .buildDocumentLoader();
-
-// const vc = require('vc-js');
-// // vc-js exports its own secure documentLoader.
-// const {defaultDocumentLoader} = vc;
-// // a valid json-ld @context.
-// const myCustomContext = require('./myCustomContext');
-//
-// const documentLoader = extendContextLoader(async url => {
-//   if(url === 'did:test:context:foo') {
-//     return {
-//       contextUrl: null,
-//       documentUrl: url,
-//       document: myCustomContext
-//     };
-//   }
-//   return defaultDocumentLoader(url);
-// });
 
 describe('Ed25519Signature2020', () => {
   describe('constructor', () => {
@@ -80,6 +58,7 @@ describe('Ed25519Signature2020', () => {
 
       expect(signed).to.have.property('proof');
       expect(signed.proof.proofValue).to
+        // eslint-disable-next-line max-len
         .equal('z5jdt64Lg9q7SqiCpnNDgNmGmwHrTYj6jTGS7xVQuYpBFfjNUtrVMfa3UobwWKRS7UVJcSivxB4T4Sr9QmmHpxy84');
     });
   });
--- test/mock-data.js
+++ test/mock-data.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2020-2021 Digital Bazaar, Inc. All rights reserved.
  */
 export const controller = 'https://example.edu/issuers/565049';
 
@@ -14,8 +14,7 @@ export const mockKey = {
 
 export const controllerDoc = {
   '@context': [
-    'https://w3id.org/security/v2',
-    // 'https://w3id.org/security/v3'
+    'https://w3id.org/security/ed25519-signature-2020/v1'
   ],
   id: 'https://example.edu/issuers/565049',
   // actual keys are going to be added in the test suite before() block
@@ -28,7 +27,7 @@ export const credential = {
   '@context': [
     'https://www.w3.org/2018/credentials/v1',
     'https://www.w3.org/2018/credentials/examples/v1',
-    'https://w3id.org/security/v3'
+    'https://w3id.org/security/ed25519-signature-2020/v1'
   ],
   id: 'http://example.edu/credentials/1872',
   type: ['VerifiableCredential', 'AlumniCredential'],
